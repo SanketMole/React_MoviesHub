7ec961ba9c26dafbbfc408476361c050b30bc86f
SanketMole <sanketmole78@gmail.com>
2025-03-21 22:44:56 +0530

    Working site

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "8106cd4e08d9fd580968192a1bf818f9";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json()
+    return data.results
+};
+export const searchMovies = async (query) => {
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+            query
+        )}`
+    );
+    const data = await response.json();
+    return data.results;
+};
+
